dad455b2c2383138f648f3ac5e4da4d17fa1301a
Abi Travers <atravers4007@gmail.com>
2017-09-24 16:07:54 +0100

    Paperclip gem working and databases migrated properly

--- app/models/picture.rb
+++
@@ -1,7 +0,0 @@
-class AddAttachmentImageToPictures < ActiveRecord::Migration[4.2]
-
-  validates :image, presence: true
-  
-  has_attached_file :image, styles: { :medium => "640x" }
-
-end
--- db/migrate/20170924095536_create_pictures.rb
+++
@@ -1,9 +0,0 @@
-class CreatePictures < ActiveRecord::Migration[5.1]
-  def change
-    create_table :pictures do |t|
-      t.string :caption
-
-      t.timestamps
-    end
-  end
-end
