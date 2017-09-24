06c66f76b92a785564ca2581350a752f487813bc
Abi Travers <atravers4007@gmail.com>
2017-09-24 20:58:17 +0100

    Test and Rspec set up properly

--- spec/features/user_can_post_picture_spec.rb
+++ spec/features/user_can_post_picture_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper.rb'
 
+RSpec.feature "Creating new post/picture", type: :feature do
+
+  scenario "user can upload a new image" do
+    visit "/"
+    click_link "New Post"
+    attach_file('Image', "spec/files/images/coffee.jpg")
+    fill_in 'Caption', with: "coffee pic, really?"
+    click_button("Create Post")
+    expect(page).to have_content("really?")
+    expect(page).to have_css("img[src*='coffee.jpg']")
+
+  end
+
+end
 # visit the root route
 # click on a button to create a new post
 # fill out the bits of the form that we need.
